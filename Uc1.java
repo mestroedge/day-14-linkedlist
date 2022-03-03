252a331f422ec76ec7ecf129d4abba527b6eaf96
mestroedge <98157564+mestroedge@users.noreply.github.com>
2022-03-03 09:33:01 -0800

    created simple linkedlist56,30,70

---
+++ Uc1.java
@@ -0,0 +1,22 @@
+package linked;
+
+public class Uc1 {
+
+public class Node {
+
+	public int key;
+	public Node next;
+
+	public Node(int key) {
+		this.key = key;
+		this.next = null;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
